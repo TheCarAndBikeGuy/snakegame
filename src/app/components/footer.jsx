2261696dba336437e4d753309f8236ba84963f84
NOAH SMART <noahsmart48@gmail.com>
2024-09-29 16:25:53 +0100

    start

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <div>
+        <p>A Snake Game Created In 2024 &#169;</p>
+      </div>
+    );
+  }
+  
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import Footer from "./components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +28,44 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <h1 className="homeHeader">Snake</h1>
+          </header>
+          <nav>
+            <Link className="home" title="Home" tabIndex={0} href="/">
+              Home
+            </Link>
+            <Link className="about" title="About" tabIndex={0} href="/About">
+              About
+            </Link>
+            <SignedIn>
+            <Link className="User" title="User" tabIndex={0} href="/User">
+              User
+            </Link>
+            <Link className="custom" title="Custom" tabIndex={0} href="/Custom">
+              Custom
+            </Link>
+            <Link className="play" title="Play" tabIndex={0} href="/Play">
+              Play
+            </Link>
+            <Link className="lb" title="Leader Board" tabIndex={0} href="/LB">
+              LB
+            </Link>
+            </SignedIn>
+          </nav>
+          {children}
+          <footer className="footer">
+            <Footer />
+            <SignedOut>
+              <SignInButton title="Sign In" />
+            </SignedOut>
+            <UserButton />
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
